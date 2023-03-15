401a57717c054e6b63bd78664eedc0394726b25d
rickyriojab <rickycela14@gmail.com>
2023-03-15 15:44:39 -0600

    Ninth commit
    vidas y monedas

---
+++ Odymensions/Assets/Scripts/MagicDust.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicDust : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OntriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
